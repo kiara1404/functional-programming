e9ad7244c3b1c41df82560fd353a83bc4c369c31
Kiara <kiarabroekhuizen1@hotmail.com>
2021-12-10 12:24:46 +0100

    first commit

---
+++ one-pager/index.js
@@ -0,0 +1,55 @@
+// --- begin scroll ---
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+};
+
+const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
+
+const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.00)) {
+            displayScrollElement(el);
+        } else if (elementOutofView(el)) {
+            hideScrollElement(el)
+        }
+    })
+}
+
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+});
+// --- einde scroll ---
+var x = document.getElementById('test');
+x.addEventListener('mouseover', playSound());
+x.addEventListener('mouseout', stopSound());
+
+function playSound(){
+    var audio = document.querySelector('#station');
+   audio.play()
+}
+
+function stopSound() {
+    var audio = document.querySelector('station');
+    audio.pause()   
+}
